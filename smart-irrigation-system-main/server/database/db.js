77277e4af5bdb6e7ee09c92f1a519c58d415ab3c
jonash-chataut <jonaschataut777@gmail.com>
2025-09-03 14:51:52 +0545

    done till error

--- smart-irrigation-system-main/server/database/db.js
+++ smart-irrigation-system-main/server/database/db.js
@@ -1,11 +1,15 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config({
+  path: require("path").resolve(__dirname, "../../../.env.local"),
+}); // load .env.local from project root
+const mongoose = require("mongoose");
 // Get MongoDB URI from environment variables
 const URI = process.env.MONGODB_URI; // connection string to MongoDB server
 
 if (!URI) {
-    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
 // Global cache to store connection status across hot reloads in development
@@ -13,24 +17,26 @@ let cached = global.mongoose || { conn: null, promise: null };
 global.mongoose = cached; // Assign cached object globally for reuse
 
 const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn; // Use existing connection if available
+  if (cached.conn) return cached.conn; // Use existing connection if available
 
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(URI, {
-            dbName: 'Irrigation',
-            bufferCommands: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            maxPoolSize:3000,
-        }).then((mongoose) => mongoose.connection)
-          .catch((error) => {
-            console.error("Failed to connect to MongoDB:", error);
-            throw new Error("Failed to connect to the database");
-        });
-    }
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(URI, {
+        dbName: "Irrigation",
+        bufferCommands: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        maxPoolSize: 3000,
+      })
+      .then((mongoose) => mongoose.connection)
+      .catch((error) => {
+        console.error("Failed to connect to MongoDB:", error);
+        throw new Error("Failed to connect to the database");
+      });
+  }
 
-    cached.conn = await cached.promise;
-    return cached.conn;
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 module.exports = connectToDatabase;
